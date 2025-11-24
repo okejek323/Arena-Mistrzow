6aabc59ee5e64ef13b20a960b8d6aecee6379ad1
oaterBTOMleko <134232228+oaterBTOMleko@users.noreply.github.com>
2025-11-24 14:55:42 +0100

    Create Bohater.cs

---
+++ Arena Mistrzow/Bohater.cs
@@ -0,0 +1,29 @@
+namespace Arena_Mistrzow
+{
+    internal class Bohater
+    {
+        class Postac
+        {
+            private string imie;
+            private int HP;
+            public string zmienne() 
+            {
+                return "0";
+            }
+        }
+        class Wojownik : Postac {
+            imie = "Wojownik";
+            private int HP;
+        }
+        class Łucznik : Postac
+        {
+            imie = "Łucznik";
+            private int HP;
+        }
+        class Mag : Postac
+        {
+            imie = "Mag";
+            private int HP;
+        }
+    }
+}
