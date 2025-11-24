ccdf8dd9ea5c0baebd933911e13a1c7a6cfd5e85
okejek323 <144665821+okejek323@users.noreply.github.com>
2025-11-24 14:54:27 +0100

    Update Program.cs

--- Arena Mistrzow/Program.cs
+++ Arena Mistrzow/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+bool alive = true;
+
+Console.WriteLine("Gracz jeden wybierz klase postaci: ");
+Console.WriteLine("1 - Wojownik");
+Console.WriteLine("2 - Łucznik");
+Console.WriteLine("3 - Mag");
+
+string wybór1 = Console.ReadLine();
+
+switch (wybór1)
+{
+    case "1":
+        Console.WriteLine("Wybrano Wojownika");
+        //Dodać wybór
+        break;
+    case "2":
+        Console.WriteLine("Wybrano Łucznik");
+        //Dodać wybór
+        break;
+    case "3":
+        Console.WriteLine("Wybrano Mag");
+        //Dodać wbór
+        break;
+}
+
+while (alive)
+{
+
+}
