e360a52a1faf4f7d17523b4e25b7c39ad5276deb
User <User@DESKTOP-4OF87C0>
2025-11-26 12:13:51 +0100

    ac

---
+++ Arena Mistrzow/Bohater.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arena_Mistrzow
+{
+    internal class Bohater
+    {
+        class Postac
+        {
+            private string imie;
+            private int HP;
+            public string zmienne() 
+            {
+                return "0";
+            }
+        }
+        class Wojownik : Postac {
+            imie = "Wojownik";
+            private int HP;
+        }
+        class Łucznik : Postac
+        {
+            imie = "Łucznik";
+            private int HP;
+        }
+        class Mag : Postac
+        {
+            imie = "Mag";
+            private int HP;
+        }
+    }
+}
